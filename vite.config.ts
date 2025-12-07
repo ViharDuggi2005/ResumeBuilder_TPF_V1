01fa402de37d5f42e54f9a419a453de537402e3e
ViharDuggi2005 <147271400+ViharDuggi2005@users.noreply.github.com>
2025-12-07 19:23:10 +0530

    Revert "die"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/ResumeBuilder_TPF_V1/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -13,7 +12,7 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(AIzaSyAmB_K5amEEQeFCrN2A7EJkhFb3RLHPuwA)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
